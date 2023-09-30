7126f27579081fbd69049795a4b6d4ed7f87c1be
bstawy <125200508@sha.edu.eg>
2023-09-30 19:36:15 +0300

    make task card slidable to perform more actions

--- lib/pages/home_view/widgets/task_item.dart
+++ lib/pages/home_view/widgets/task_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TaskItem extends StatelessWidget {
   const TaskItem({super.key});
@@ -9,56 +10,100 @@ class TaskItem extends StatelessWidget {
     var theme = Theme.of(context);
 
     return Container(
-      width: mediaQuery.width,
-      height: 115,
       margin: const EdgeInsets.symmetric(horizontal: 33, vertical: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       decoration: BoxDecoration(
-        color: Colors.white,
+        gradient: LinearGradient(
+          colors: [
+            Colors.teal,
+            Color(0xffEC4B4B),
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          stops: const [0.5, 0.5],
+        ),
         borderRadius: BorderRadius.circular(15),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            width: 5,
-            decoration: BoxDecoration(
-              color: theme.colorScheme.primary,
+      child: Slidable(
+        startActionPane: ActionPane(
+          motion: ScrollMotion(),
+          extentRatio: 0.25,
+          children: [
+            SlidableAction(
+              onPressed: (context) {},
+              backgroundColor: Colors.teal,
+              foregroundColor: Colors.white,
               borderRadius: BorderRadius.circular(15),
+              icon: Icons.edit,
+              label: 'Edit',
             ),
+          ],
+        ),
+        endActionPane: ActionPane(
+          motion: ScrollMotion(),
+          extentRatio: 0.25,
+          children: [
+            SlidableAction(
+              onPressed: (context) {},
+              backgroundColor: Color(0xffEC4B4B),
+              foregroundColor: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              icon: Icons.delete,
+              label: 'Delete',
+            ),
+          ],
+        ),
+        child: Container(
+          width: mediaQuery.width,
+          height: 115,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                'Play basket ball',
-                style: theme.textTheme.titleSmall!
-                    .copyWith(color: theme.colorScheme.primary),
+              Container(
+                width: 5,
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.primary,
+                  borderRadius: BorderRadius.circular(15),
+                ),
               ),
-              const SizedBox(height: 8),
-              Row(
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Icon(Icons.alarm),
                   Text(
-                    '10:30 AM',
-                    style: theme.textTheme.bodyMedium,
+                    'Play basket ball',
+                    style: theme.textTheme.titleSmall!
+                        .copyWith(color: theme.colorScheme.primary),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      const Icon(Icons.alarm),
+                      Text(
+                        '10:30 AM',
+                        style: theme.textTheme.bodyMedium,
+                      ),
+                    ],
                   ),
                 ],
               ),
+              Container(
+                width: 70,
+                height: 35,
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.primary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Image.asset('assets/images/check_icon.png'),
+              ),
             ],
           ),
-          Container(
-            width: 70,
-            height: 35,
-            decoration: BoxDecoration(
-              color: theme.colorScheme.primary,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Image.asset('assets/images/check_icon.png'),
-          ),
-        ],
+        ),
       ),
     );
   }
