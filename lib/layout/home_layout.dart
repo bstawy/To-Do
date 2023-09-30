75f14b5737a0a8e58a0dbaec80bea7622033fc12
bstawy <125200508@sha.edu.eg>
2023-09-30 18:01:19 +0300

    not finish home view

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,5 +1,7 @@
 import 'package:calendar_timeline/calendar_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:todo/pages/home_view/home_view.dart';
+import 'package:todo/pages/settings/settings_view.dart';
 
 class HomeLayout extends StatefulWidget {
   static const String routeName = 'home-layout-view';
@@ -10,44 +12,23 @@ class HomeLayout extends StatefulWidget {
 }
 
 class _HomeLayoutState extends State<HomeLayout> {
+  int selectedIndex = 0;
+  List<Widget> pages = const [HomeView(), SettingsView()];
+
   @override
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context).size;
     var theme = Theme.of(context);
 
     return Scaffold(
-      body: Stack(
-        alignment: Alignment(0, 2.1),
-        children: [
-          Container(
-            width: mediaQuery.width,
-            height: 200,
-            padding: EdgeInsets.only(
-              top: 65,
-              left: 52,
-            ),
-            color: theme.primaryColor,
-            child: Text(
-              'To Do List',
-              style: theme.textTheme.titleLarge,
-            ),
-          ),
-          CalendarTimeline(
-            initialDate: DateTime.now(),
-            firstDate: DateTime.now(),
-            lastDate: DateTime(2024, 12, 31),
-            onDateSelected: (date) => print(date),
-            leftMargin: 20,
-            monthColor: theme.colorScheme.secondary,
-            dayColor: Colors.teal[100],
-            activeDayColor: theme.colorScheme.primary,
-            activeBackgroundDayColor: Colors.white,
-            dotsColor: Colors.white,
-            selectableDayPredicate: (date) => date.day != 23,
-          )
-        ],
-      ),
+      body: pages[selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             icon: ImageIcon(
---
+++ lib/pages/home_view/home_view.dart
@@ -0,0 +1,44 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context).size;
+    var theme = Theme.of(context);
+
+    return Stack(
+      alignment: const Alignment(0, 2.1),
+      children: [
+        Container(
+          width: mediaQuery.width,
+          height: 200,
+          padding: const EdgeInsets.only(
+            top: 65,
+            left: 52,
+          ),
+          color: theme.primaryColor,
+          child: Text(
+            'To Do List',
+            style: theme.textTheme.titleLarge,
+          ),
+        ),
+        CalendarTimeline(
+          initialDate: DateTime.now(),
+          firstDate: DateTime.now(),
+          lastDate: DateTime(2024, 12, 31),
+          onDateSelected: (date) => print(date),
+          leftMargin: 20,
+          monthColor: theme.colorScheme.secondary,
+          dayColor: Colors.teal[100],
+          activeDayColor: theme.colorScheme.primary,
+          activeBackgroundDayColor: Colors.white,
+          dotsColor: Colors.white,
+          selectableDayPredicate: (date) => date.day != 23,
+        )
+      ],
+    );
+  }
+}
