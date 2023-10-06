194f5f79e7410b2ad31ee09fa48302061c504315
bstawy <125200508@sha.edu.eg>
2023-10-06 18:08:29 +0300

    doesn't have any logic

--- lib/core/provider/app_provider.dart
+++ lib/core/provider/app_provider.dart
@@ -1,36 +1,5 @@
 import 'package:flutter/material.dart';
 
 class AppProvider extends ChangeNotifier {
-  bool isLanguageBottomSheetVisible = false;
-  bool isThemeBottomSheetVisible = false;
-  bool isTaskAdderVisible = false;
-  changeLanguageSheetStatus(String status) {
-    if (status == 'open') {
-      isLanguageBottomSheetVisible = true;
-      isThemeBottomSheetVisible = false;
-    } else {
-      isLanguageBottomSheetVisible = false;
-    }
-    notifyListeners();
-  }
 
-  changeThemeSheetStatus(String status) {
-    if (status == 'open') {
-      isThemeBottomSheetVisible = true;
-      isLanguageBottomSheetVisible = false;
-    } else {
-      isThemeBottomSheetVisible = false;
-    }
-    notifyListeners();
-  }
-
-  addNewTask() {
-    isTaskAdderVisible = true;
-    notifyListeners();
-  }
-
-  createNewTask() {
-    isTaskAdderVisible = false;
-    notifyListeners();
-  }
 }
