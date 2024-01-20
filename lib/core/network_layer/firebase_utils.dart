a720a1ed3cbf65b14b42cbd662d4a1f0bc343401
bastawy <125200508@sha.edu.eg>
2024-01-20 03:37:17 +0200

    - Add login function which Authenticate with FirebaseAuth

--- lib/core/network_layer/firebase_utils.dart
+++ lib/core/network_layer/firebase_utils.dart
@@ -1,10 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:todo/core/provider/app_provider.dart';
 import 'package:todo/core/utils/extract_date.dart';
 
 import '../../model/task_model.dart';
 
-class FirestoreUtils {
+class FirebaseUtils {
+  static Future<Either<String, UserCredential>> logIn(
+      String email, String password) async {
+    late UserCredential user;
+    try {
+      user = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "invalid-credential") {
+        return Left(e.code);
+      }
+    }
+    return Right(user);
+  }
+
+  static logOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 
   static CollectionReference<TaskModel> getCollection() {
     return FirebaseFirestore.instance
@@ -35,14 +56,17 @@ class FirestoreUtils {
     return docRef.update(task.toFirestore());
   }
 
-  static Future <List<TaskModel>> getDataFromFirestore() async {
+  static Future<List<TaskModel>> getDataFromFirestore() async {
     var snapshot = await getCollection().get();
     return snapshot.docs.map((element) => element.data()).toList();
   }
 
-  static Stream<QuerySnapshot<TaskModel>> getRealTimeDataFromFirestore(DateTime date) {
+  static Stream<QuerySnapshot<TaskModel>> getRealTimeDataFromFirestore(
+      DateTime date) {
     var reqDate = ExtractDate.extractDate(date);
-    var snapshot = getCollection().where('date', isEqualTo: reqDate.millisecondsSinceEpoch).snapshots();
+    var snapshot = getCollection()
+        .where('date', isEqualTo: reqDate.millisecondsSinceEpoch)
+        .snapshots();
     return snapshot;
   }
 }
