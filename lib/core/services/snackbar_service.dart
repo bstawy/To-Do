92759ed1d791846c9ec54c4bebc3dbc1d4239678
bastawy-admin <125200508@sha.edu.eg>
2024-01-22 18:07:49 +0200

    - add AlertMessageSnackbar - change Duration of Snackbar to 2 seconds

--- lib/core/services/snackbar_service.dart
+++ lib/core/services/snackbar_service.dart
@@ -80,7 +80,90 @@ class SnackBarService {
         );
       },
       duration: const Duration(
-        seconds: 3,
+        seconds: 2,
+      ),
+      dismissDirections: [DismissDirection.endToStart],
+    );
+  }
+
+  static void showAlertMessage(String msg) {
+
+    BotToast.showCustomNotification(
+      toastBuilder: (void Function() cancelFunc) {
+        return Material(
+          color: Colors.transparent,
+          child: Container(
+            width: double.maxFinite,
+            height: msg.length > 80 ? 150 : 95,
+            padding: const EdgeInsets.all(16),
+            margin: const EdgeInsets.only(
+              left: 24,
+              right: 24,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: msg.length > 80
+                ? Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Text(
+                    msg,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontFamily: 'SF pro',
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: IconButton(
+                    onPressed: cancelFunc,
+                    icon: const Icon(
+                      Icons.close_rounded,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            )
+                : Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Text(
+                    msg,
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.fade,
+                    maxLines: 3,
+                    style: const TextStyle(
+                      fontFamily: 'SF pro',
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: IconButton(
+                    onPressed: cancelFunc,
+                    icon: const Icon(
+                      Icons.close_rounded,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      duration: const Duration(
+        seconds: 2,
       ),
       dismissDirections: [DismissDirection.endToStart],
     );
@@ -165,7 +248,7 @@ class SnackBarService {
         );
       },
       duration: const Duration(
-        seconds: 3,
+        seconds: 2,
       ),
       dismissDirections: [DismissDirection.endToStart],
     );
