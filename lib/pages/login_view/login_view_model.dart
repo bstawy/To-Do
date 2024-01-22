4452dcd559e0c0ad01bb8230d0d619f19bfb6867
bastawy-admin <125200508@sha.edu.eg>
2024-01-22 19:34:55 +0200

    - enhance login view model

--- lib/pages/login_view/login_view_model.dart
+++ lib/pages/login_view/login_view_model.dart
@@ -6,37 +6,54 @@ import '../../core/provider/app_provider.dart';
 
 class LoginViewModel extends ChangeNotifier {
   String? _userID;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
+  String? _loginStatus = "";
+
+  String? get userID => _userID;
+  GlobalKey<FormState>? get formKey => _formKey;
+  TextEditingController? get email => _email;
+  TextEditingController? get password => _password;
+  String? get loginStatus => _loginStatus;
 
   LoginViewModel() {
     _userID = AppProvider.userID;
   }
 
-  String? get userID => _userID;
-  TextEditingController? get email => _email;
-  TextEditingController? get password => _password;
+  login() async {
+    if (_formKey.currentState!.validate()) {
+      final response = await FirebaseUtils.logIn(_email.text, _password.text);
+      response.fold(
+        (l) {
+          _loginStatus = 'invalid-credential';
+        },
+        (r) {
+          final UserCredential user = r;
+          if (isVerified(user)) {
+            storeUserID(user.user!.uid);
+            _loginStatus = "success";
+          } else {
+            _loginStatus = "email-not-verified";
+          }
+        },
+      );
+    }
+    if (_loginStatus!.isEmpty) _loginStatus = "invalid";
+    notifyListeners();
+  }
+
+  bool isVerified(UserCredential user) {
+    if (user.user!.emailVerified) {
+      return true;
+    }
+    return false;
+  }
 
   storeUserID(String id) async {
     _userID = id;
-    AppProvider.userID = id;
+    AppProvider.userID = _userID;
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString("UID", _userID!);
-  }
-
-  Future<String> login({required var formKey}) async {
-    String loginStatus = "";
-    if (formKey.currentState!.validate()) {
-      final response = await FirebaseUtils.logIn(_email.text, _password.text);
-      response.fold((l) {
-        loginStatus = 'invalid-credential';
-      }, (r) {
-        final UserCredential user = r;
-        storeUserID(user.user!.uid);
-        loginStatus =  "success";
-      });
-    }
-    if (loginStatus.isEmpty) loginStatus = "invalid";
-    return loginStatus;
   }
 }
