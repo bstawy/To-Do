2f8d6f5c7016038cfb6921e134e2381041f57b0e
bstawy <125200508@sha.edu.eg>
2024-01-23 20:35:51 +0200

    - create UI for change password button - create UI for delete account button

--- lib/pages/settings/settings_view.dart
+++ lib/pages/settings/settings_view.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../../core/provider/app_provider.dart';
 import '../../pages/settings/widgets/language_bottom_sheet.dart';
 import '../../pages/settings/widgets/settings_item.dart';
 import '../../pages/settings/widgets/theme_bottom_sheet.dart';
-
 
 class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
@@ -21,6 +21,7 @@ class _SettingsViewState extends State<SettingsView> {
     var appProvider = Provider.of<AppProvider>(context);
 
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           width: mediaQuery.width,
@@ -37,20 +38,106 @@ class _SettingsViewState extends State<SettingsView> {
         ),
         const SizedBox(height: 25),
         SettingsItem(
-          settingOptionTitle: 'language',
-          selectedOption: 'English',
-          onClicked: () {
-            showLanguageBottomSheet(context);
-          },
-        ),
-        const SizedBox(height: 40),
-        SettingsItem(
           settingOptionTitle: 'Theme',
           selectedOption: appProvider.isDarkMode() ? 'Dark' : 'Light',
           onClicked: () {
             showThemeBottomSheet(context);
           },
         ),
+        const SizedBox(height: 32),
+        Divider(
+          color: theme.colorScheme.primary,
+          thickness: 2,
+          indent: 100,
+          endIndent: 75,
+        ),
+        const SizedBox(height: 32),
+        Padding(
+          padding: const EdgeInsets.only(left: 38.0),
+          child: Text(
+            "Account settings",
+            style: theme.textTheme.titleMedium,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 40, left: 56.0, bottom: 17),
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: theme.colorScheme.secondary,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    side: BorderSide(
+                      color: theme.primaryColor,
+                      width: 1,
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  minimumSize: Size(mediaQuery.width / 1.28, 50),
+                  maximumSize: Size(mediaQuery.width / 1.28, 50),
+                ),
+                child: Row(
+                  children: [
+                    Text('Change password', style: theme.textTheme.bodyLarge),
+                    const Spacer(),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      color: theme.colorScheme.onSecondary,
+                      size: 18,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: theme.colorScheme.secondary,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    side: const BorderSide(
+                      color: Color(0xffEE0E0E),
+                      width: 1,
+                    ),
+                  ),
+                  foregroundColor: const Color(0xffEE0E0E),
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  minimumSize: Size(mediaQuery.width / 1.28, 50),
+                  maximumSize: Size(mediaQuery.width / 1.28, 50),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      'Delete account',
+                      style: theme.textTheme.bodyLarge!.copyWith(
+                        color: const Color(0xffEE0E0E),
+                      ),
+                    ),
+                    const Spacer(),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      color: theme.colorScheme.onSecondary,
+                      size: 18,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        /*
+        SettingsItem(
+          settingOptionTitle: 'language',
+          selectedOption: 'English',
+          onClicked: () {
+            showLanguageBottomSheet(context);
+          },
+        ),
+        */
       ],
     );
   }
