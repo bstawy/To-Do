6b0c9a9113f761ea0c5f40ad90645162456675ca
bstawy <125200508@sha.edu.eg>
2024-01-24 00:48:57 +0200

    - use SettingsViewModel to reset password

--- lib/pages/settings/settings_view.dart
+++ lib/pages/settings/settings_view.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:provider/provider.dart';
 
 import '../../core/provider/app_provider.dart';
-import '../../pages/settings/widgets/language_bottom_sheet.dart';
+import '../../core/services/snackbar_service.dart';
+import '../../pages/login_view/login_view.dart';
+import '../../pages/settings/settings_view_model.dart';
+import '../../pages/settings/widgets/custom_elevated_button.dart';
 import '../../pages/settings/widgets/settings_item.dart';
-import '../../pages/settings/widgets/theme_bottom_sheet.dart';
 
 class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
@@ -14,145 +19,106 @@ class SettingsView extends StatefulWidget {
 }
 
 class _SettingsViewState extends State<SettingsView> {
+  late SettingsViewModel settingsViewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    settingsViewModel = SettingsViewModel();
+  }
+
   @override
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context).size;
     var theme = Theme.of(context);
     var appProvider = Provider.of<AppProvider>(context);
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: mediaQuery.width,
-          height: 200,
-          padding: const EdgeInsets.only(
-            top: 65,
-            left: 52,
-          ),
-          color: theme.primaryColor,
-          child: Text(
-            'Settings',
-            style: theme.textTheme.titleLarge,
-          ),
-        ),
-        const SizedBox(height: 25),
-        SettingsItem(
-          settingOptionTitle: 'Theme',
-          selectedOption: appProvider.isDarkMode() ? 'Dark' : 'Light',
-          onClicked: () {
-            showThemeBottomSheet(context);
-          },
-        ),
-        const SizedBox(height: 32),
-        Divider(
-          color: theme.colorScheme.primary,
-          thickness: 2,
-          indent: 100,
-          endIndent: 75,
-        ),
-        const SizedBox(height: 32),
-        Padding(
-          padding: const EdgeInsets.only(left: 38.0),
-          child: Text(
-            "Account settings",
-            style: theme.textTheme.titleMedium,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 40, left: 56.0, bottom: 17),
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: theme.colorScheme.secondary,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    side: BorderSide(
-                      color: theme.primaryColor,
-                      width: 1,
-                    ),
+    return ChangeNotifierProvider(
+      create: (context) => settingsViewModel,
+      builder: (context, child) {
+        return Consumer<SettingsViewModel>(
+          builder: (context, vm, child) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: mediaQuery.width,
+                  height: 200,
+                  padding: const EdgeInsets.only(
+                    top: 65,
+                    left: 52,
                   ),
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  minimumSize: Size(mediaQuery.width / 1.28, 50),
-                  maximumSize: Size(mediaQuery.width / 1.28, 50),
-                ),
-                child: Row(
-                  children: [
-                    Text('Change password', style: theme.textTheme.bodyLarge),
-                    const Spacer(),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      color: theme.colorScheme.onSecondary,
-                      size: 18,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: theme.colorScheme.secondary,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    side: const BorderSide(
-                      color: Color(0xffEE0E0E),
-                      width: 1,
-                    ),
+                  color: theme.primaryColor,
+                  child: Text(
+                    'Settings',
+                    style: theme.textTheme.titleLarge,
                   ),
-                  foregroundColor: const Color(0xffEE0E0E),
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  minimumSize: Size(mediaQuery.width / 1.28, 50),
-                  maximumSize: Size(mediaQuery.width / 1.28, 50),
                 ),
-                child: Row(
-                  children: [
-                    Text(
-                      'Delete account',
-                      style: theme.textTheme.bodyLarge!.copyWith(
-                        color: const Color(0xffEE0E0E),
+                const SizedBox(height: 25),
+                SettingsItem(
+                  settingOptionTitle: 'Theme',
+                  selectedOption: vm.isDarkMode() ? 'Dark' : 'Light',
+                  onClicked: () {
+                    vm.showThemeBottomSheet(context, vm);
+                  },
+                ),
+                const SizedBox(height: 32),
+                Divider(
+                  color: theme.colorScheme.primary,
+                  thickness: 2,
+                  indent: 100,
+                  endIndent: 75,
+                ),
+                const SizedBox(height: 32),
+                Padding(
+                  padding: const EdgeInsets.only(left: 38.0),
+                  child: Text(
+                    "Account settings",
+                    style: theme.textTheme.titleMedium,
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 40, left: 56.0, bottom: 17),
+                  child: Column(
+                    children: [
+                      CustomELevatedButton(
+                          text: 'Change password',
+                          onTap: () async {
+                            EasyLoading.show();
+                            await vm.changePassword();
+                            EasyLoading.dismiss();
+
+                            if (vm.changePasswordStatus == "success") {
+                              SnackBarService.showAlertMessage(
+                                  'A password reset link has been sent to your email');
+                              Timer(const Duration(seconds: 3), () {
+                                Navigator.pushNamedAndRemoveUntil(context,
+                                    LoginView.routeName, (route) => false);
+                              });
+                            } else {
+                              SnackBarService.showErrorMessage(
+                                  '${vm.changePasswordStatus}');
+                            }
+                          }),
+                      const SizedBox(height: 30),
+                      CustomELevatedButton(
+                        text: 'Delete account',
+                        textStyle: theme.textTheme.bodyLarge!.copyWith(
+                          color: const Color(0xffEE0E0E),
+                        ),
+                        borderColor: const Color(0xffEE0E0E),
+                        foregroundColor: const Color(0xffEE0E0E),
+                        onTap: () {},
                       ),
-                    ),
-                    const Spacer(),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      color: theme.colorScheme.onSecondary,
-                      size: 18,
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-        /*
-        SettingsItem(
-          settingOptionTitle: 'language',
-          selectedOption: 'English',
-          onClicked: () {
-            showLanguageBottomSheet(context);
+              ],
+            );
           },
-        ),
-        */
-      ],
-    );
-  }
-
-  void showLanguageBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) => const LanguageBottomSheetWidget(),
-    );
-  }
-
-  void showThemeBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) => const ThemeBottomSheetWidget(),
+        );
+      },
     );
   }
 }
