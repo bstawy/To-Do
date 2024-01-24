9d6e24899768f9fe978ca4e1873df86784306889
bstawy <125200508@sha.edu.eg>
2024-01-24 18:22:19 +0200

    - remove unused imports

--- lib/my_application.dart
+++ lib/my_application.dart
@@ -1,13 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:bot_toast/bot_toast.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:provider/provider.dart';
+
 import 'core/provider/app_provider.dart';
 import 'core/theme/application_theme.dart';
 import 'layout/home_layout.dart';
+import 'pages/home_view/edit_task.dart';
 import 'pages/login_view/login_view.dart';
 import 'pages/register_view/register_view.dart';
-import 'pages/home_view/edit_task.dart';
 
 class MyApplication extends StatelessWidget {
   const MyApplication({super.key});
@@ -19,7 +20,7 @@ class MyApplication extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "To Do",
-      themeMode: appProvider.currentTheme,
+      themeMode: AppProvider.currentTheme,
       theme: ApplicationTheme.lightTheme,
       darkTheme: ApplicationTheme.darkTheme,
       initialRoute: appProvider.logging(),
