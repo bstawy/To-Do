50f84d9335d14de4fe36326cc0890092d2b47c2b
bstawy <125200508@sha.edu.eg>
2024-01-24 18:34:01 +0200

    - handle application theme changing through AppProvider

--- lib/pages/settings/widgets/theme_bottom_sheet.dart
+++ lib/pages/settings/widgets/theme_bottom_sheet.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../settings_view_model.dart';
+import '../../../core/provider/app_provider.dart';
 import 'selected_option_widget.dart';
 import 'unselected_option_widget.dart';
 
 class ThemeBottomSheetWidget extends StatefulWidget {
-  final SettingsViewModel settingsViewModel;
-
   const ThemeBottomSheetWidget({
     super.key,
-    required this.settingsViewModel,
   });
 
   @override
@@ -20,37 +17,35 @@ class ThemeBottomSheetWidget extends StatefulWidget {
 class _ThemeBottomSheetWidgetState extends State<ThemeBottomSheetWidget> {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: widget.settingsViewModel,
-      builder: (context, child) {
-        return Container(
-          height: 350,
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  widget.settingsViewModel.changeTheme(ThemeMode.light);
-                  Navigator.pop(context);
-                },
-                child: widget.settingsViewModel.isDarkMode()
-                    ? const UnselectedOption(unselectedOptionTitle: 'Light')
-                    : const SelectedOption(selectedOptionTitle: 'Light'),
-              ),
-              const SizedBox(height: 20),
-              GestureDetector(
-                  onTap: () {
-                    widget.settingsViewModel.changeTheme(ThemeMode.dark);
-                    Navigator.pop(context);
-                  },
-                  child: widget.settingsViewModel.isDarkMode()
-                      ? const SelectedOption(selectedOptionTitle: 'Dark')
-                      : const UnselectedOption(unselectedOptionTitle: 'Dark')),
-            ],
+    var appProvider = Provider.of<AppProvider>(context);
+
+    return Container(
+      height: 350,
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () {
+              appProvider.changeTheme(ThemeMode.light);
+              Navigator.pop(context);
+            },
+            child: appProvider.isDarkMode()
+                ? const UnselectedOption(unselectedOptionTitle: 'Light')
+                : const SelectedOption(selectedOptionTitle: 'Light'),
           ),
-        );
-      },
+          const SizedBox(height: 20),
+          GestureDetector(
+            onTap: () {
+              appProvider.changeTheme(ThemeMode.dark);
+              Navigator.pop(context);
+            },
+            child: appProvider.isDarkMode()
+                ? const SelectedOption(selectedOptionTitle: 'Dark')
+                : const UnselectedOption(unselectedOptionTitle: 'Dark'),
+          ),
+        ],
+      ),
     );
   }
 }
